05d1e365aa66d493309fb034308f1e84023fc39a
binapani9556 <125079858+binapani9556@users.noreply.github.com>
2023-04-17 02:45:37 +0530

    Update Fula.java

--- src/test/java/mavenproject/Fula.java
+++ src/test/java/mavenproject/Fula.java
@@ -10,22 +10,6 @@ public class Fula {
 		System.out.println();
 
 //hey fula
-}
-
-}
-
-package mavenproject;
-
-import org.testng.annotations.Test;
-
-public class Fula {
-
-	@Test
-	public static void AddPrint() {
-		int a=10;
-		int b=20;
-		System.out.println("a+b");
-		System.out.println();
 //kokoo====
 }
 
