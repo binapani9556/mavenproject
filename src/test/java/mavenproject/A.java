3c107ff8faa785dc9011d85bfbf3717d5eaed936
binapani9556 <125079858+binapani9556@users.noreply.github.com>
2023-08-13 00:15:43 +0530

    modified A,class

--- src/test/java/mavenproject/A.java
+++ src/test/java/mavenproject/A.java
@@ -6,27 +6,25 @@ import org.testng.annotations.Test;
 public class A {
 	public A() {
 		System.out.println("constructpr A executed");
-		}
+	}
+
 	public A(int a) {
-		
+
 		System.out.println("constructpr A int [ara executed");
-		}
-	
-	
-	
-	
+	}
 
 	public void method1() {
 		System.out.println("method1 non parameter  executed");
 	}
+
 	public void method1(int a) {
 		System.out.println("method1 int parameter  executed");
 	}
-	
+
 	public void method1(int a, int b) {
 		System.out.println("method1 int int  parameter  executed");
 	}
-	
+
 	public void method1(int a, char b) {
 		System.out.println("method1 int char parameter  executed");
 	}
@@ -34,15 +32,19 @@ public class A {
 	public void method1(char a, char b) {
 		System.out.println("method1 non parameter  executed");
 	}
-@Test	
+
+	public void method4() {
+		System.out.println("==========hi bina====");
+	}
+
+	@Test
 //public static void main(String[] args) {
-public static void displayGudi() {
-	A objA = new A(10);
-	//objA.method1();
-	System.out.println("baba+gudi");
-	System.out.println("fula+gudi");
-	System.out.println("modify ");
-}
+	public static void displayGudi() {
+		A objA = new A(10);
+		// objA.method1();
+		System.out.println("baba+gudi");
+		System.out.println("fula+gudi");
+		System.out.println("modify ");
+	}
 
 }
-
